ab7ec9e3ba47acdca20ae7f3cec9b230e8dc92bd
MrEmpy <71796063+MrEmpy@users.noreply.github.com>
2022-09-04 12:27:07 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,6 +9,7 @@ main() {
     printf "\n\033[0;34m[*] \033[0;37mInstalling...\n"
     apt-get install -y osslsigncode python3 python3-pip
     pip install -r requirements.txt
+    chmod +x condor.py
     printf '\n\033[0;32m[+] \033[0;37mInstallation completed!'
 }
 
